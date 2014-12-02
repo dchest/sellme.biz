5404d8e35b9fd711ad2ec1b0a37eb2d51f952096
Dmitry Chestnykh <dmitry@codingrobots.com>
2014-12-02 08:59:09 +0100

    Get rid of most of inline JavaScript.
    This is a work-in-progress to remove 'unsafe-inline' from
    Content-Security-Policy.
    
    Still left:
    
     - pages/password/ and pages/password2/
    
       We need to exclude these locations from CSP, because they are
       one-file HTML+JS things, which use inline JS.
    
     - Google Analytics:
    
       While in a separate JS file (assets/js/google-analytics.js), it still
       loads external JS by putting "<script>" into DOM, which is not
       allowed with CSP if we remove 'unsafe-inline'. We could just do
       <script src="https://www.google-analytics.com/analytics.js"></script>
       but we load Analytics conditionally, dynamically, to be able to
       exclude it from localhost (not important) and .onion domains.
       We should figure out how to statically exclude it from .onion without
       having two copies of the website for .biz and .onion.

---
+++ assets/js/google-analytics.js
@@ -0,0 +1,16 @@
+//
+// XXX This script loads external JS by adding <script> tag to DOM.
+//
+// Do not include Analytics on localhost or .onion domain (Tor mirror).
+//
+if (!document.domain.match(/(^localhost)|(\.onion)$/)) {
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-71330-2', 'sellme.biz');
+  ga('send', 'pageview');
+
+}
